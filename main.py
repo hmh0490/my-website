f764be8aaab4941e8ae807041d5e68e1ceb01510
hmh0490 <hajni0490@gmail.com>
2023-05-09 09:56:31 +0200

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpg") # width=600
+
+with col2:
+    st.title("Hoang Minh Hanh")
+    content = """
+    Hi, I am Hanh!
+     
+    I am an accomplished finance professional with a diverse international background in banking and finance. 
+    I have gained extensive experience in various roles, allowing me to develop a comprehensive understanding of 
+    financial markets and strategies. Notably, I have successfully passed the rigorous examination for the Chartered 
+    Financial Analyst (CFA) certificate, highlighting my commitment to professional excellence.
+    
+    Throughout my career, I have developed a strong passion for programming and have acquired valuable skills in this
+    area. I also proactively pursued programming skills to complement my finance expertise. Through self-study and 
+    practical application, I have acquired a strong foundation in programming languages relevant to the finance sector.
+    
+    I am now driven to combine my profound knowledge of finance with my programming capabilities, aiming to leverage
+    technology to optimize financial processes, automate tasks, and develop sophisticated models for data analysis 
+    and forecasting, and deliver innovative solutions.
+    """
+    # st.write(content)
+    st.info(content)
