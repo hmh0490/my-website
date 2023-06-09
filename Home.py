58953ee57bb62049597288d5af9659ac42858596
hmh0490 <hajni0490@gmail.com>
2023-06-09 22:45:22 +0200

    Add initial_sidebar_state

--- Home.py
+++ Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
 col1, col2 = st.columns(2)
 
 with col1:
